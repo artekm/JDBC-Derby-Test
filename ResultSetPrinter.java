abed40793acd28b81749eb62e2f99085b7c6ea42
artekm <44265668+artekm@users.noreply.github.com>
2019-08-10 11:07:12 +0200

    Optimisation

--- ResultSetPrinter.java
+++ ResultSetPrinter.java
@@ -1,35 +1,12 @@
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ResultSetPrinter {
 	public void printAll(ResultSet rs) throws SQLException {
-		int numColumns = rs.getMetaData().getColumnCount();
-
-		List<String> dataFields = new ArrayList<>();
-
-		for (int col = 1; col <= numColumns; col++)
-			dataFields.add(rs.getMetaData().getColumnName(col));
-
-		while (rs.next())
-			for (int col = 1; col <= numColumns; col++)
-				dataFields.add(rs.getString(col));
-
-		int[] columnsWidth = calculateColumnsWidth(dataFields, numColumns);
-		String underline = prepareUnderline(columnsWidth);
-
-		int pos = 0;
-		for (String name : dataFields) {
-			System.out.printf(" %" + columnsWidth[pos % numColumns] + "s |", name);
-			pos++;
-			if (pos % numColumns == 0) {
-				System.out.println();
-				if (pos / numColumns == 1)
-					System.out.println(underline);
-			}
-		}
-		System.out.println(underline);
+		System.out.println(formatAll(rs));
 	}
 
 	public String formatAll(ResultSet rs) throws SQLException {
@@ -51,7 +28,7 @@ public class ResultSetPrinter {
 
 		int pos = 0;
 		for (String name : dataFields) {
-			result=result.append(String.format(" %" + columnsWidth[pos % numColumns] + "s |", name));
+			result = result.append(String.format(" %" + columnsWidth[pos % numColumns] + "s |", name));
 			pos++;
 			if (pos % numColumns == 0) {
 				result.append("\n");
@@ -59,10 +36,13 @@ public class ResultSetPrinter {
 					result = result.append(underline).append("\n");
 			}
 		}
-		result = result.append(underline);
+		
+		if (pos > numColumns)
+			result = result.append(underline);
+		
 		return result.toString();
 	}
-
+	
 	private int[] calculateColumnsWidth(List<String> dataFields, int numColumns) {
 		int[] columnsWidth = new int[numColumns];
 		int pos = 0;
